e364a06a0d6a2b974d4930f2c689855d140d5e66
unknown <briannac@shaw.ca>
2021-07-08 22:15:14 +0100

    create IntervalButtons component

---
+++ src/Components/IntervalButtonGroup.tsx
@@ -0,0 +1,35 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function IntervalButtonGroup() {
+  const classes = useStyles();
+  const intervalArray: string[] = [
+    "P1",
+    "m2",
+    "M2",
+    "m3",
+    "M3",
+    "P4",
+    "P5",
+    "m6",
+    "M6",
+    "m7",
+    "M7",
+    "P8",
+  ]; //should we do diminished and augmented too? Doesn't matter for aural, but could for visual
+  const intervalButtons = intervalArray.map((interval) => (
+    <Button style={{ textTransform: "none" }} variant="contained">
+      {interval}
+    </Button>
+  ));
+
+  return <div className={classes.root}>{intervalButtons}</div>;
+}
