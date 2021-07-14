d8abd18362fddc2efda32d067c86c55f84905c56
unknown <briannac@shaw.ca>
2021-07-14 22:09:37 +0100

    add new components to ExercisePage

--- src/Components/ExercisePage.tsx
+++ src/Components/ExercisePage.tsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
 import IntervalButtonGroup from "./IntervalButtonGroup";
+import Counter from "./Counter";
+import Exercise from "./Exercise";
+import Answer from "./Answer";
 
-export default function ExercisePage() {
+import "./ExercisePage.css";
+
+const ExercisePage: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+  const [displayExercise, setDisplayExercise] = useState(true);
+
+  const increment = function () {
+    setCounter(counter + 1);
+  };
+
+  const checkAnswer = function (event: React.MouseEvent<HTMLButtonElement>) {
+    const { target } = event;
+    if (target) console.log((target as HTMLButtonElement).value);
+  };
+
+  const toggleExercise = function () {
+    setDisplayExercise(!displayExercise);
+  };
+
   return (
-    <div>
-      <IntervalButtonGroup />
-    </div>
+    <main className="container">
+      <div className="counter">
+        <Counter counter={counter} />
+      </div>
+
+      <div>
+        {displayExercise ? (
+          <Exercise exercise="testexercise" />
+        ) : (
+          <Answer answer="true" />
+        )}
+      </div>
+      <div className="intervalButtons">
+        <IntervalButtonGroup checkAnswer={checkAnswer} />
+      </div>
+    </main>
   );
-}
+};
+export default ExercisePage;
