b2f5120d6cc2a304e65b1a5c5d94a038a1826b41
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-07-29 17:25:46 -0700

    Update home page linking

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,13 +9,15 @@ import './Landing.css'
 
 function Landing() {
   return (
-    <div>
+    <div id="landing">
       <header className="App-header">
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" >
-              Sight Singing
-            </Typography>
+            <Link to="/" className="home">
+              <Typography variant="h6" >
+                Sight Singing
+                </Typography>
+              </Link>
 
             <Button color="inherit">
               <NavLink to="/visual" activeClassName="visual">
@@ -53,50 +55,51 @@ function Landing() {
             <Sing />
           </Route>
 
+          <Route exact path="/">
+            <section className="cards">
+
+              <Grid container spacing={3}>
+                
+                <Grid item xs={4}>
+                  <Link to="/visual">
+                    <IntervalCard
+                      image="https://images.unsplash.com/photo-1591025810539-a321000cda85?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hlZXQlMjBtdXNpY3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                      imageTitle="Sheet music"
+                      title="Visual Intervals"
+                      description="View two notes written in sheet music and guess the interval."
+                    />
+                  </Link>
+                </Grid>
+                
+
+                
+                  <Grid item xs={4}>
+                    <Link to="/aural">
+                      <IntervalCard
+                        image="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGVhZHBob25lc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                        imageTitle="A pair of headphones"
+                        title="Aural Intervals"
+                        description="Hear two notes side-by-side and guess the interval."
+                      />
+                    </Link>
+                  </Grid>
+                
+                  <Grid item xs={4}>
+                    <Link to="/sing">
+                      <IntervalCard image="https://media.istockphoto.com/photos/singer-microphone-vocal-talent-music-show-recital-picture-id1144547914?k=6&m=1144547914&s=612x612&w=0&h=PoGWtQbDzy9LOse62j2Ny_laAeY1MQ4nM-G_9DSeOYo="
+                        imageTitle="A person singing into a microphone"
+                        title="Vocal Intervals"
+                        description="It's your turn to sight-sing the intervals and see if your pitch is right!."
+                      />
+                    </Link>
+                  </Grid>
+                
+              </Grid>
+            </section>
+            
+          </Route>
+
         </Switch>
-        <section className="cards">
-
-          <Grid container spacing={3}>
-            
-            <Grid item xs={4}>
-              <Link to="/visual">
-                <IntervalCard
-                  image="https://images.unsplash.com/photo-1591025810539-a321000cda85?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hlZXQlMjBtdXNpY3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
-                  imageTitle="Sheet music"
-                  title="Visual Intervals"
-                  description="View two notes written in sheet music and guess the interval."
-                />
-              </Link>
-            </Grid>
-            
-
-            
-              <Grid item xs={4}>
-                <Link to="/aural">
-                  <IntervalCard
-                    image="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGVhZHBob25lc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
-                    imageTitle="A pair of headphones"
-                    title="Aural Intervals"
-                    description="Hear two notes side-by-side and guess the interval."
-                  />
-                </Link>
-              </Grid>
-            
-
-            
-              <Grid item xs={4}>
-                <Link to="/sing">
-                  <IntervalCard image="https://media.istockphoto.com/photos/singer-microphone-vocal-talent-music-show-recital-picture-id1144547914?k=6&m=1144547914&s=612x612&w=0&h=PoGWtQbDzy9LOse62j2Ny_laAeY1MQ4nM-G_9DSeOYo="
-                    imageTitle="A person singing into a microphone"
-                    title="Vocal Intervals"
-                    description="It's your turn to sight-sing the intervals and see if your pitch is right!."
-                  />
-                </Link>
-              </Grid>
-            
-          </Grid>
-        </section>
-
 
       </main>
     </div>
