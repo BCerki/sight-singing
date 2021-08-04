fd110702e4628519767b209408f9ad7a0e626656
unknown <briannac@shaw.ca>
2021-08-04 17:30:40 +0100

    refactor interval generation

--- src/Components/ExercisePage.tsx
+++ src/Components/ExercisePage.tsx
@@ -7,9 +7,13 @@ import Answer from "./Answer";
 //playing sound
 import * as Tone from "tone";
 //music theory
-import { Note, Interval, Distance, Scale, Chord } from "tonal";
+// import { Interval, Distance, Scale, Chord } from "tonal";
+import { Note } from "@tonaljs/tonal";
 
 import "./ExercisePage.css";
+
+import exercise from "../Helpers/createExercise";
+const { firstNote, secondNote, interval } = exercise;
 
 //lodash array methods
 const _ = require("lodash");
@@ -34,178 +38,24 @@ const ExercisePage: React.FC = () => {
     userAnswer: "",
     isCorrect: null,
   };
-
   const [answerState, setAnswerState] = useState(initialAnswerState);
 
   //interval state
-  const notes = [
-    "Ab3",
-    "A3",
-    "A#3",
-    "Bb3",
-    "B3",
-    "Cb3",
-    "C3",
-    "C#3",
-    "Db3",
-    "D3",
-    "D#3",
-    "Eb3",
-    "E3",
-    "Fb3",
-    "F3",
-    "F#3",
-    "Gb3",
-    "G3",
-    "G#3",
-    "Ab4",
-    "A4",
-    "A#4",
-    "Bb4",
-    "B4",
-    "Cb4",
-    "C4",
-    "C#4",
-    "Db4",
-    "D4",
-    "D#4",
-    "Eb4",
-    "E4",
-    "Fb4",
-    "F4",
-    "F#4",
-    "Gb4",
-    "G4",
-    "G#4",
-    "Ab5",
-    "A5",
-    "A#5",
-    "Bb5",
-    "B5",
-    "Cb5",
-    "C5",
-    "C#5",
-    "Db5",
-    "D5",
-    "D#5",
-    "Eb5",
-    "E5",
-    "Fb5",
-    "F5",
-    "F#5",
-    "Gb5",
-    "G5",
-    "G#5",
-  ];
-
-  const firstNoteIndex: number = _.sample(_.range(notes.length - 1));
-  const firstNote = notes[firstNoteIndex];
-
-  const getSecondNoteIndex = function (
-    firstNoteIndex: number,
-    notesArray: string[]
-  ) {
-    let secIndex: undefined | number = undefined;
-    while (!secIndex || secIndex < 0 || secIndex > notesArray.length) {
-      secIndex = firstNoteIndex + _.sample(_.range(-22, 22)); //22 keeps the interval from being larger than a P8
-    }
-
-    return secIndex;
-  };
-
-  const secondNoteIndex = getSecondNoteIndex(firstNoteIndex, notes);
-  const secondNote = notes[secondNoteIndex];
-  // console.log(
-  //   "getSecondNoteIndex is",
-  //   secondNoteIndex,
-  //   "secondNote is",
-  //   secondNote
-  // );
-
-  const interval = Distance.interval(firstNote, secondNote);
-  //reformat and simplify Tonal's intervals to our button ones
-  const stringInterval: string = interval as string;
-  const formattedInterval: string | string[] = stringInterval
-    .split("")
-    .reverse()
-    .join("")
-    .replace("-", "");
-
-  const eqInterval = {
-    d2: "P1",
-    A1: "m2",
-    d3: "M2",
-    A2: "m3",
-    d4: "M3",
-    A3: "P4",
-    d5: "A4",
-    d6: "P5",
-    A5: "m6",
-    d7: "M6",
-    A6: "m7",
-    d8: "M7",
-    A7: "P8",
-  };
-
-  //still need to add double dim and aug FIX FIX
-  // switch (formattedInterval) {
-  //   case "d2":
-  //     equivalentInterval = "P1";
-  //     break;
-  //   case "A1":
-  //     equivalentInterval = "m2";
-  //     break;
-  //   case "d3":
-  //     equivalentInterval = "M2";
-  //     break;
-  //   case "A2":
-  //     equivalentInterval = "m3";
-  //     break;
-  //   case "d4":
-  //     equivalentInterval = "M3";
-  //     break;
-  //   case "A3":
-  //     equivalentInterval = "P4";
-  //     break;
-  //   case "d5":
-  //     equivalentInterval = "A4";
-  //     break;
-  //   case "d6":
-  //     equivalentInterval = "P5";
-  //     break;
-  //   case "A5":
-  //     equivalentInterval = "m6";
-  //     break;
-  //   case "d7":
-  //     equivalentInterval = "M6";
-  //     break;
-  //   case "A6":
-  //     equivalentInterval = "m7";
-  //     break;
-  //   case "d8":
-  //     equivalentInterval = "M7";
-  //     break;
-  //   case "A7":
-  //     equivalentInterval = "P8";
-  //     break;
-  //   default:
-  //     equivalentInterval = formattedInterval;
-  // }
-
   const newIntervalState: IntervalState = {
     firstNote,
     secondNote,
-    interval: "placeholder",
+    interval,
   };
-
   const [intervalState, setIntervalState] = useState(newIntervalState);
+
   //counter state
   const [counter, setCounter] = useState(0);
 
   const increment = function () {
     setCounter(counter + 1);
   };
-  //helper functions
+
+  //aural exercise functions
   const playAuralExercise = function () {
     const synth = new Tone.Synth().toDestination();
     const now = Tone.now();
@@ -223,6 +73,7 @@ const ExercisePage: React.FC = () => {
     synth.triggerAttackRelease(secondNote, "4n", now + 1);
   };
 
+  //general exercise functions
   const checkAnswer = function (event: React.MouseEvent) {
     const correctInterval = intervalState.interval;
     const userInterval = event.currentTarget.textContent;
